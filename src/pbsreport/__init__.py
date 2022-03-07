97e8e950fbb4b41d33289e77e0832957c3f37676
Renato Teixeira <rena2damas@gmail.com>
2022-03-07 19:26:28 +0000

    Set version in package

--- src/pbsreport/__init__.py
+++ src/pbsreport/__init__.py
@@ -1,1 +1,9 @@
+try:
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata  # python<=3.7
+
 from .cli import cli
+
+
+__version__ = metadata.version("pbsreport")
