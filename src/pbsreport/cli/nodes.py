d4a2a08445a6552d8f9aca8e9a828380ce46f0f1
Renato Teixeira <rena2damas@gmail.com>
2022-03-09 14:23:18 +0000

    Display output from formatter

--- src/pbsreport/cli/nodes.py
+++ src/pbsreport/cli/nodes.py
@@ -1,8 +1,10 @@
 import click
 
+from pbsreport.api.pbs import PBSFormatter
+
 
 @click.command()
 @click.pass_obj
 def nodes(pbs):
     """Provide metrics from cluster nodes."""
-    pbs.nodes()
+    PBSFormatter.nodes(data=pbs.nodes())
