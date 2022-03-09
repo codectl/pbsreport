29fbe24f720032ba1f409dd391e33bd3c9e51619
Renato Teixeira <rena2damas@gmail.com>
2022-03-09 13:55:55 +0000

    Include schema for resources

--- src/pbsreport/schemas/pbs.py
+++ src/pbsreport/schemas/pbs.py
@@ -3,28 +3,27 @@ from marshmallow import Schema, EXCLUDE, fields, post_load, pre_load
 
 __all__ = ("NodeSchema", "NodesSchema")
 
+class ResourceSchema(Schema):
+    dloc = fields.String(load_default=None)
+    arch = fields.String(load_default=None)
+    cpu_type = fields.String(load_default=None)
+    node_type = fields.String(load_default=None)
+    network = fields.String(load_default=None)
+    cpus = fields.Integer(data_key="ncpus", load_default=0)
+    gpus = fields.Integer(data_key="ngpus", load_default=0)
+    mem = fields.String(load_default="0kb")
+
+    class Meta:
+        unknown = EXCLUDE
 
 class NodeSchema(Schema):
-    fqdn = fields.String(data_key='Mom')
-    state = fields.String()
-    comment = fields.String()
-    queue = fields.String()
-    resources_available = fields.Nested(Schema.from_dict({
-        "dloc": fields.String(),
-        "arch": fields.String(),
-        "cpu_type": fields.String(),
-        "node_type": fields.String(),
-        "network": fields.String(),
-        "cpus": fields.Integer(data_key='ncpus', load_default=0),
-        "gpus": fields.Integer(data_key='ngpus', load_default=0),
-        "mem": fields.String(load_default="0kb")
-    }), unknown=EXCLUDE)
-    resources_assigned = fields.Nested(Schema.from_dict({
-        "cpus": fields.Integer(data_key='ncpus', load_default=0),
-        "gpus": fields.Integer(data_key='ngpus', load_default=0),
-        "mem": fields.String(load_default="0kb")
-    }), unknown=EXCLUDE)
-    jobs = fields.List(fields.String())
+    fqdn = fields.String(data_key="Mom", load_default=None)
+    state = fields.String(load_default=None)
+    comment = fields.String(load_default=None)
+    queue = fields.String(load_default=None)
+    resources_available = fields.Nested(ResourceSchema)
+    resources_assigned = fields.Nested(ResourceSchema, only=["cpus", "gpus", "mem"])
+    jobs = fields.List(fields.String(), load_default=[])
 
     class Meta:
         unknown = EXCLUDE
