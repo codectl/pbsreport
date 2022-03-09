899811ef0d56174ada5be8d7fdf6a9c5144aa006
Renato Teixeira <rena2damas@gmail.com>
2022-03-09 18:27:16 +0000

    Tests for human_size

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,5 +22,14 @@ def test_convert_bytes():
 
 
 def test_bytes_as_int():
-    assert utils.bytes_as_int("1000b") == 1000
-    assert utils.bytes_as_int("1000kb") == 1000
+    assert utils.remove_units("1000b") == 1000
+    assert utils.remove_units("1000kb") == 1000
+
+
+def test_human_size():
+    assert utils.human_size(0) == "0b"
+    assert utils.human_size(1) == "1b"
+    assert utils.human_size(1024) == "1Kb"
+    assert utils.human_size(1024 ** 2) == "1Mb"
+    assert utils.human_size(1024 ** 3) == "1Gb"
+    assert utils.human_size(1024 ** 8) == "1Yb"
