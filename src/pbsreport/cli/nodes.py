16bde3f7f989047ebaae190e4cdc618e571cf835
Renato Teixeira <rena2damas@gmail.com>
2022-03-10 11:01:30 +0000

    Add option to sort results by

--- src/pbsreport/cli/nodes.py
+++ src/pbsreport/cli/nodes.py
@@ -4,8 +4,18 @@ from pbsreport.api.pbs import PBSFormatter
 
 
 @click.command()
+@click.option(
+    "--sort",
+    "sort",
+    help="column to sort by",
+    type=click.Choice(
+        ["name", "queue", "state", "cpus", "gpus", "mem", "cpu_type", "comment"]
+    ),
+    default="name",
+    show_default=True,
+)
 @click.pass_obj
-def nodes(pbs):
+def nodes(pbs, sort):
     """Provide metrics from cluster nodes."""
-    stdout = PBSFormatter.nodes(data=pbs.nodes())
+    stdout = PBSFormatter.nodes(data=pbs.nodes(sort=sort))
     print(stdout)
