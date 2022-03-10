db2b69311686dad4be53f58bd5d7dc74b1b73339
Renato Teixeira <rena2damas@gmail.com>
2022-03-10 16:25:35 +0000

    Swap order of parameters

--- src/pbsreport/cli/nodes.py
+++ src/pbsreport/cli/nodes.py
@@ -14,7 +14,7 @@ from pbsreport.api.pbs import PBSFormatter
 )
 @click.argument("name", nargs=1, type=str, default="")
 @click.pass_obj
-def nodes(pbs, sort, name):
+def nodes(pbs, name, sort):
     """Provide metrics from cluster nodes."""
     stdout = PBSFormatter.nodes(data=pbs.nodes(name=name, sort=sort))
     print(stdout)
@@ -31,7 +31,7 @@ def nodes(pbs, sort, name):
 )
 @click.argument("name", nargs=1, type=str, default="")
 @click.pass_obj
-def vnodes(pbs, sort, name):
+def vnodes(pbs, name, sort):
     """Provide metrics from cluster vnodes."""
     stdout = PBSFormatter.nodes(data=pbs.nodes(name=name, vnodes=True, sort=sort))
     print(stdout)
